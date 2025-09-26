7e38612952d001681ca04d7f16b696f5554a9abe
mmuratardag <mmadatsci@gmail.com>
2025-09-26 17:53:17 +0200

    fixed the file name

---
+++ jsieve/__init__.py
@@ -0,0 +1,10 @@
+"""
+JSieve - JavaScript Extractor & Enumerator
+A powerful tool for web security analysis
+"""
+
+from .main import main
+
+__version__ = "0.1.0"
+__author__ = "M. Murat Ardag"
+__license__ = "MIT"
